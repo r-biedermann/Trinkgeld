5d7b1723143f0231e3680315df78b0ea4a205caf
Raphael <raphael.biedermann@tobit.com>
2020-09-07 18:08:50 +0200

    add background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,7 @@ import { hot } from 'react-hot-loader/root';
 import React, { PureComponent } from 'react';
 
 // Components
-import Headline from './header/headline/Headline';
-import Intro from './header/intro/Intro';
-import Todos from './todos/Todos';
+import Main from './main/Main';
 
 // We use PureComponent instead of Component because it handles the shouldComponentUpdate method for us.
 // If we want to define our own shouldComponentUpdate logic we have to use Component instead of PureComponent.
@@ -12,9 +10,7 @@ class App extends PureComponent {
     render() {
         return (
             <>
-                <Headline headline="chayns® React Todo Example"/>
-                <Intro intro="This is a very simple and basic example of how u can use chayns® and React together."/>
-                <Todos/>
+                <Main/>
             </>
         );
     }
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react';
+import './main.scss';
+
+class Main extends PureComponent {
+    render() {
+        return (
+            <div className="container">
+                <div className="background1"/>
+                <div className="background2"/>
+                <div className="background3"/>
+                <div className="background4"/>
+            </div>
+        );
+    }
+}
+
+export default Main;
