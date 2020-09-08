47e334903bc7b874368d28a771a639cbdf15052f
Raphael <raphael.biedermann@tobit.com>
2020-09-08 16:34:24 +0200

    add header

---
+++ src/components/main/content/donations/Aquarium.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class Aquarium extends PureComponent {
+    render() {
+        return (
+            <p>text</p>
+        );
+    }
+}
+
+export default Aquarium;
--- src/components/main/content/donations/Donations.jsx
+++ src/components/main/content/donations/Donations.jsx
@@ -1,9 +1,22 @@
 import React, { PureComponent } from 'react';
+import Aquarium from './Aquarium';
+import './donations.scss';
 
 class Donations extends PureComponent {
     render() {
         return (
-            <p>text</p>
+            <div>
+                <div className="header">
+                    <div className="artist">
+                        <h1>Thomas Mayer</h1>
+                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+                    </div>
+                    <div className="qr">
+                        <img src="https://tappqa.tobit.com/training/2020/RB/images/qrCode.png" alt="" className="image"/>
+                    </div>
+                </div>
+                <Aquarium/>
+            </div>
         );
     }
 }
