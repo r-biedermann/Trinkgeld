121e21da7ded99c99aaf502b9704be3bc48b3d91
Raphael <raphael.biedermann@tobit.com>
2020-09-08 17:31:00 +0200

    gitflow-feature-stash: useData

--- src/components/main/DummyList.jsx
+++ src/components/main/DummyList.jsx
@@ -1,64 +1,64 @@
 const Dummyist = [{
-    name: "Bill Gates",
-    pictureURL: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Bill_Gates_2018.jpg",
+    name: 'Bill Gates',
+    pictureURL: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Bill_Gates_2018.jpg',
     tipAmount: 10,
 }, {
-    name: "Barack Obama",
-    pictureURL: "https://convent.de/content/uploads/2019/07/Barack_Obama-580x580.jpg",
+    name: 'Barack Obama',
+    pictureURL: 'https://convent.de/content/uploads/2019/07/Barack_Obama-580x580.jpg',
     tipAmount: 5,
 }, {
-    name: "Usain Bolt",
-    pictureURL: "https://tmssl.akamaized.net/images/portrait/originals/611239-1535449084.jpg",
+    name: 'Usain Bolt',
+    pictureURL: 'https://tmssl.akamaized.net/images/portrait/originals/611239-1535449084.jpg',
     tipAmount: 2.5,
 }, {
-    name: "Jeff Bezos",
-    pictureURL: "https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg",
+    name: 'Jeff Bezos',
+    pictureURL: 'https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg',
     tipAmount: 10,
 }, {
-    name: "Michael Wendler",
-    pictureURL: "https://www.watson.de/imgdb/068e/Qx,A,67,269,535,593,295,439,118,175/4711249516902430",
+    name: 'Michael Wendler',
+    pictureURL: 'https://www.watson.de/imgdb/068e/Qx,A,67,269,535,593,295,439,118,175/4711249516902430',
     tipAmount: 1,
 }, {
-    name: "Lionel Messi",
-    pictureURL: "https://www.grenzecho.net/sites/default/files/dpistyles_v2/ena_16_9_extra_big/2020/07/03/node_38404/7662675/public/2020/07/03/B9723912505Z.1_20200703110919_000+G62G973GP.1-0.jpg?itok=CPVPfvTw1593767366",
+    name: 'Lionel Messi',
+    pictureURL: 'https://www.grenzecho.net/sites/default/files/dpistyles_v2/ena_16_9_extra_big/2020/07/03/node_38404/7662675/public/2020/07/03/B9723912505Z.1_20200703110919_000+G62G973GP.1-0.jpg?itok=CPVPfvTw1593767366',
     tipAmount: 1,
 }, {
-    name: "Joachim Löw",
-    pictureURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/20180602_FIFA_Friendly_Match_Austria_vs._Germany_Jogi_L%C3%B6w_850_1386.jpg/1200px-20180602_FIFA_Friendly_Match_Austria_vs._Germany_Jogi_L%C3%B6w_850_1386.jpg",
+    name: 'Joachim Löw',
+    pictureURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/20180602_FIFA_Friendly_Match_Austria_vs._Germany_Jogi_L%C3%B6w_850_1386.jpg/1200px-20180602_FIFA_Friendly_Match_Austria_vs._Germany_Jogi_L%C3%B6w_850_1386.jpg',
     tipAmount: 1,
 }, {
-    name: "Gaben Newell",
-    pictureURL: "https://images.derstandard.at/img/2019/07/03/Valve.png?w=750&s=afcbbba8",
+    name: 'Gaben Newell',
+    pictureURL: 'https://images.derstandard.at/img/2019/07/03/Valve.png?w=750&s=afcbbba8',
     tipAmount: 10,
 }, {
-    name: "Zeus",
-    pictureURL: "https://www.alaturka.info/images/geschichte/zeus.jpg",
+    name: 'Zeus',
+    pictureURL: 'https://www.alaturka.info/images/geschichte/zeus.jpg',
     tipAmount: 10,
 }, {
-    name: "Keanu Reeves",
-    pictureURL: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Reuni%C3%A3o_com_o_ator_norte-americano_Keanu_Reeves_%28cropped%29.jpg",
-    tipAmount: 2.50,
+    name: 'Keanu Reeves',
+    pictureURL: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Reuni%C3%A3o_com_o_ator_norte-americano_Keanu_Reeves_%28cropped%29.jpg',
+    tipAmount: 10,
 }, {
-    name: "Shary",
-    pictureURL: "https://presse.wdr.de/plounge/_mdb_generated/presselounge2012/presselounge/bilder/2017/06/13/1_Shary_Reeves_Jochen_Manz_256.jpg",
+    name: 'Shary',
+    pictureURL: 'https://presse.wdr.de/plounge/_mdb_generated/presselounge2012/presselounge/bilder/2017/06/13/1_Shary_Reeves_Jochen_Manz_256.jpg',
     tipAmount: 1,
 }, {
-    name: "Aiman Abdallah",
-    pictureURL: "https://www.muensterschezeitung.de/var/storage/images/wn/startseite/sport/weltsport/sonstige/3538034-qualifikation-zur-wm-2019-aiman-abdallah-moderiert-deutsche-rugby-wm-hoffnungsspiele/99203858-1-ger-DE/Qualifikation-zur-WM-2019-Aiman-Abdallah-moderiert-deutsche-Rugby-WM-Hoffnungsspiele_image_1024_width.jpg",
+    name: 'Aiman Abdallah',
+    pictureURL: 'https://www.muensterschezeitung.de/var/storage/images/wn/startseite/sport/weltsport/sonstige/3538034-qualifikation-zur-wm-2019-aiman-abdallah-moderiert-deutsche-rugby-wm-hoffnungsspiele/99203858-1-ger-DE/Qualifikation-zur-WM-2019-Aiman-Abdallah-moderiert-deutsche-Rugby-WM-Hoffnungsspiele_image_1024_width.jpg',
     tipAmount: 1,
 }, {
-    name: "Jahn Böhmermann",
-    pictureURL: "https://images.derstandard.at/img/2020/09/05/bohmermann.jpg?w=750&s=b3f50675",
+    name: 'Jahn Böhmermann',
+    pictureURL: 'https://images.derstandard.at/img/2020/09/05/bohmermann.jpg?w=750&s=b3f50675',
     tipAmount: 1,
 }, {
-    name: "Pablo Picasso",
-    pictureURL: "https://upload.wikimedia.org/wikipedia/commons/9/98/Pablo_picasso_1.jpg",
+    name: 'Pablo Picasso',
+    pictureURL: 'https://upload.wikimedia.org/wikipedia/commons/9/98/Pablo_picasso_1.jpg',
     tipAmount: 2.5,
 }, {
-    name: "Neil Armstrong",
-    pictureURL: "https://img.welt.de/img/vermischtes/mobile112442892/7242503027-ci102l-w1024/Jahresrueckblick-Todesfaelle-2012.jpg",
+    name: 'Neil Armstrong',
+    pictureURL: 'https://img.welt.de/img/vermischtes/mobile112442892/7242503027-ci102l-w1024/Jahresrueckblick-Todesfaelle-2012.jpg',
     tipAmount: 5,
-}, 
-]
+},
+];
 
-export default DummyList
+export default DummyList;
