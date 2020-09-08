0e67ad069dc131fbc57aec6462d7f597d9b01fe4
Raphael <raphael.biedermann@tobit.com>
2020-09-08 12:14:00 +0200

    add footerbar

---
+++ src/components/main/content/footer/Donator.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class Donator extends PureComponent {
+    render() {
+        return (
+            <p>Name</p>
+        );
+    }
+}
+
+export default Donator;
--- src/components/main/content/footer/Footer.jsx
+++ src/components/main/content/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import './footer.scss';
+import Donator from './Donator';
 
 class Footer extends PureComponent {
     render() {
@@ -7,7 +8,13 @@ class Footer extends PureComponent {
             <div className="footer">
                 <div>
                     <h1>Trinkgeld</h1>
-
+                    <p>Name</p>
+                </div>
+                <div className="list">
+                    <Donator className="donator"/>
+                    <Donator className="donator"/>
+                    <Donator className="donator"/>
+                    <Donator className="donator"/>
                 </div>
             </div>
         );
