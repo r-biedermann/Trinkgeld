d76f8ec54f9cdfc3d0801de79925ee629e64e4d3
Leopold <leopold.pinkernell@tobit.com>
2020-09-09 17:56:16 +0200

    :lipstick: Fix Eslint Errors

--- src/components/main/content/donations/IconContainer.jsx
+++ src/components/main/content/donations/IconContainer.jsx
@@ -1,24 +1,27 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import './IconContainer.scss';
 
 function IconContainer(props) {
-    return(
-        <div className="containertest" style={{
-            animation: `path${props.index} 120s ease-in-out infinite alternate`
-        }}>
+    return (
+        <div
+            className="containertest"
+            style={{
+                animation: `path${props.index} 120s ease-in-out infinite alternate`,
+            }}
+        >
             <div className="imgOverlay">
-                <img className="icon" src={props.imgURL}/>
+                <img className="icon" src={props.imgURL} alt=""/>
                 <svg viewBox="0 -45 100 100" className="svg">
                     <path id="curve" d="m 0 0 a 1 1 0 0 0 100 0" fill="transparent"/>
                     <text width="100" className="name" textAnchor="middle">
-                        <textPath alignment-baseline="top" href="#curve" startOffset="50%">
+                        <textPath alignmentBaseline="top" href="#curve" startOffset="50%">
                             {props.name}
                         </textPath>
                     </text>
                 </svg>
             </div>
         </div>
-    )
+    );
 }
 
 export default IconContainer;
