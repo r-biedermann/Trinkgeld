eb17248b5264d1706357372aca8c17fb3473a991
Raphael <raphael.biedermann@tobit.com>
2020-09-09 14:55:01 +0200

    finish animation

--- src/components/main/content/donations/Donations.jsx
+++ src/components/main/content/donations/Donations.jsx
@@ -3,6 +3,34 @@ import Aquarium from './Aquarium';
 import './donations.scss';
 
 class Donations extends PureComponent {
+    constructor() {
+        super();
+        this.state = {
+            funfact: [
+                'Thomas Mayer kann mit seinem Daumen 2500kg heben.',
+                'Thomas Mayer wurde in einem Stall geboren, den er selbst gebaut hat.',
+            ],
+            number: 0,
+            interval: {},
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            interval: setInterval(this.getNumber(), 240000),
+        });
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.state.interval);
+    }
+
+    getNumber() {
+        this.setState({
+            number: Math.floor(Math.random() * this.state.funfact.length),
+        });
+    }
+
     render() {
         return (
             <div>
@@ -10,16 +38,17 @@ class Donations extends PureComponent {
                     <div className="artist">
                         <h1>Thomas Mayer</h1>
                         {/* eslint-disable-next-line max-len */}
-                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+                        <p>Wusstest du schon?</p>
+                        {this.state.funfact[this.state.number]}
                     </div>
                     <div className="qr">
-                        <img src="https://tappqa.tobit.com/training/2020/RB/images/qrCode.png" alt="" className="image"/>
+                        <img src="https://tappqa.tobit.com/training/2020/RB/images/qrCode.png" alt="" className="image" />
                     </div>
                     <div className="position">
                         <div className="splashtext">Trinkgeld</div>
                     </div>
                 </div>
-                <Aquarium/>
+                <Aquarium />
             </div>
         );
     }
