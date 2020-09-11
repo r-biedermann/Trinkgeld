92ed3fc7b50dc1a23ab01521648fe53b5b439a4d
Raphael <raphael.biedermann@tobit.com>
2020-09-11 11:27:07 +0200

    display correctly on runtime

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,9 +3,28 @@ import './main.scss';
 import Content from './content/Content';
 
 class Main extends PureComponent {
+    constructor() {
+        super();
+        this.state = {
+            windowHeight: 0,
+        };
+    }
+
+    useWindowHeight() {
+        const retVal = chayns.addWindowMetricsListener((e) => {
+            if (e !== this.statewindowHeight) {
+                this.setState({ windowHeight: e });
+            }
+        }, true);
+        return () => chayns.removeWindowMetricsListener(retVal);
+    }
+
     render() {
         return (
-            <div className="container">
+            <div
+                className="container"
+                style={{ height: `${this.state.windowHeight}` }}
+            >
                 <div className="background1 animation"/>
                 <div className="background2 animation"/>
                 <div className="background3 animation"/>
